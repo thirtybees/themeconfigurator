cad3e3decd77a70106febbb79065b019181dbad0
Markus Hitter <mah@jump-ing.de>
2018-12-13 18:54:51 +0100

    Make release 3.0.8.

--- themeconfigurator.php
+++ themeconfigurator.php
@@ -62,7 +62,7 @@ class ThemeConfigurator extends Module
     {
         $this->name = 'themeconfigurator';
         $this->tab = 'front_office_features';
-        $this->version = '3.0.7';
+        $this->version = '3.0.8';
         $this->author = 'thirty bees';
         $this->need_instance = 0;
         $this->bootstrap = true;
